2d24b55a9c03a5cfade51ca66f43d3637a4fa406
Micheal Harker <micheal@michealharker.com>
2017-01-18 22:17:29 +0000

    fix many bugs, including those on github. closes #1

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import config from '../config.json'
 
 import { connect } from 'coffea'
 import dude from 'debug-dude'
+import { decode } from 'ent'
 import request from 'request'
 import xml2js from 'xml2js'
 
@@ -40,14 +41,15 @@ const respondInline = (evt, reply) => {
         let id = `anime${entry.id[0]}`
         let image = entry.image[0]
         let title = `${entry.title[0]} (${entry.start_date[0].slice(0, 4)})`
-        let description = entry.synopsis[0].replace(/(<|\[)(?:.|\n)*?(\]|>)/gm, '')
+        let english = entry.english[0]
+        let description = decode(entry.synopsis[0].replace(/(<|\[)(?:.|\n)*?(\]|>)/gm, ''))
 
         answer.push({
           type: 'article', id, title, description,
           url: image,
           thumb_url: image,
           input_message_content: {
-            message_text: `<b>${title}</b> <a href='${image}'>(Image)</a>\n\n<pre>${description}</pre>`,
+            message_text: `<a href='${image}'>⁣</a><b>${title}</b>\n\n<b>English:</b> <i>${english}</i>\n<b>Episodes:</b> <i>${entry.episodes[0]}</i>\n<b>Score:</b> <i>⭐ ${entry.score[0]}</i>\n<b>Status:</b> <i>${entry.status[0]}</i>\n\n<pre>${description}</pre>\n\n<a href='https://myanimelist.net/anime/${entry.id[0]}'>See more on MyAnimeList &gt;</a>`,
             parse_mode: 'HTML'
           }
         })
